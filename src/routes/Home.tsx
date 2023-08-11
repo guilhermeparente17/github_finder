f3901c688f0b75addd6e204b5e9f3c0500b782af
Guilherme Parente <guilherme.parente17@outlook.com>
2023-08-11 11:25:59 -0300

    ajeitando config para vercel

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Search from '../components/Serach/Search'
 import { UserProps } from '../types/user';
 import User from '../components/User/User';
