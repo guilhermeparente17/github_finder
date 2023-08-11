1d40becfe9e664f6dd561dcf9470ac1394b82692
Guilherme Parente <guilherme.parente17@outlook.com>
2023-08-11 11:18:22 -0300

    first commit

---
+++ src/routes/Home.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Search from '../components/Serach/Search'
+import { UserProps } from '../types/user';
+import User from '../components/User/User';
+import Error from '../components/Error/Error';
+
+const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
+
+    const loadUser = async(userName: string) => {
+        setError(false);
+        setUser(null);
+
+        const response = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await response.json();
+
+        if (response.status === 404) {
+            setError(true);
+            return;
+        }
+
+        const {avatar_url, login, location, followers, following} = data;
+
+        const userData: UserProps = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following
+        }
+
+        setUser(userData);
+    }
+    return (
+        <div>
+            <Search loadUser={loadUser} />
+            {user && <User {...user} />}
+            {error && <Error />}
+        </div>
+    )
+}
+
+export default Home
